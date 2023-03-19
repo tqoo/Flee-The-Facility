6bfd9682c177b1a1bb980d3faa6364cd933e66ff
Comix Inc <56793799+tqoo@users.noreply.github.com>
2023-03-19 09:18:18 +0000

    Added enemies, random pathfinding, inititating fight scene

--- main.ts
+++ main.ts
@@ -6,6 +6,9 @@ enum ActionKind {
     WalkUp,
     WalkDown
 }
+namespace SpriteKind {
+    export const Enemy_Blob = SpriteKind.create()
+}
 function InitwalkLeft () {
     walkleftanim = animation.createAnimation(ActionKind.Walking, 100)
     imgstilleft = assets.image`PlayerIdle`
@@ -50,29 +53,57 @@ function initVertAnim () {
     animation.attachAnimation(mySprite, walkDownAnim)
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (mySprite.tileKindAt(TileDirection.Top, assets.tile`dungeonBook`)) {
-        game.showLongText("It's a book. \\n Or is it a journal?", DialogLayout.Bottom)
-        game.showLongText("Either way, the pages are illegible anyways.", DialogLayout.Bottom)
-    }
-    if (mySprite.tileKindAt(TileDirection.Top, assets.tile`TrapMemo`)) {
-        game.showLongText("It's a memo to \"Adam Kittle\" Would you like to read it?", DialogLayout.Bottom)
-        if (game.ask("Read the memo?")) {
-            game.showLongText(`"Adam, we just finished putting in those tiles you ordered, and they SUCK. They're so fragile that that... thing could easily just slip through and escape. We're gonna put in the flooring anyways, but when that thing ecapes, dont say I didn't warn you. 
- Kind regards, Joe Hills."`, DialogLayout.Bottom)
+    if (level == 1) {
+        if (mySprite.tileKindAt(TileDirection.Top, assets.tile`dungeonBook`)) {
+            game.showLongText("It's a book. \\n Or is it a journal?", DialogLayout.Bottom)
+            game.showLongText("Either way, the pages are illegible anyways.", DialogLayout.Bottom)
         }
-    }
-    if (mySprite.tileKindAt(TileDirection.Center, assets.tile`trapCracked`)) {
-        game.showLongText("There's a cracked tile underneath you. \\n If you jump on it, it could break?", DialogLayout.Bottom)
-        if (game.ask("Step on the tile?")) {
-            for (let index = 0; index < 3; index++) {
-                mySprite.vy += -12.5
-                mySprite.ay = 25
-                pauseUntil(() => mySprite.tileKindAt(TileDirection.Center, assets.tile`trapCracked`))
-                mySprite.ay = 0
+        if (mySprite.tileKindAt(TileDirection.Top, assets.tile`TrapMemo`)) {
+            game.showLongText("It's a memo to \"Adam Kittle\" Would you like to read it?", DialogLayout.Bottom)
+            if (game.ask("Read the memo?")) {
+                game.showLongText(`"Adam, we just finished putting in those tiles you ordered, and they SUCK. They're so fragile that that... thing could easily just slip through and escape. We're gonna put in the flooring anyways, but when that thing ecapes, dont say I didn't warn you. 
+ Kind regards, Joe Hills."`, DialogLayout.Bottom)
+            }
+        }
+        if (mySprite.tileKindAt(TileDirection.Center, assets.tile`trapCracked`)) {
+            game.showLongText("There's a cracked tile underneath you. \\n If you jump on it, it could break?", DialogLayout.Bottom)
+            if (game.ask("Step on the tile?")) {
+                game.splash("You jumped on the tile.")
+                game.splash("...")
+                game.splash("...")
+                game.splash("...")
+                game.splash("It broke!")
+                color.FadeToBlack.startScreenEffect(1000)
+                color.pauseUntilFadeDone()
+                tiles.setCurrentTilemap(tilemap`level2`)
+                color.startFadeFromCurrent(color.originalPalette)
             }
         }
     }
 })
+function changeLevelFunctions () {
+    enemies = []
+    if (level == 2) {
+        for (let index = 0; index < 4; index++) {
+            enemies.push(sprites.create(assets.image`Blob`, SpriteKind.Enemy_Blob))
+        }
+        for (let value of enemies) {
+            tiles.placeOnRandomTile(value, assets.tile`officeTile1`)
+        }
+    }
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy_Blob, function (sprite, otherSprite) {
+    game.showLongText("You collide with a... blob?", DialogLayout.Bottom)
+    FIGHT(otherSprite)
+})
+function FIGHT (enemy: Sprite) {
+    scene.centerCameraAt(0, 0)
+    tiles.setCurrentTilemap(tilemap`fightScene`)
+    for (let value of enemies) {
+        sprites.destroy(value)
+    }
+}
+let enemies: Sprite[] = []
 let walkDownAnim: animation.Animation = null
 let walkUpAnim: animation.Animation = null
 let walkrightanim: animation.Animation = null
@@ -81,13 +112,33 @@ let imgstilleft: Image = null
 let walkleftanim: animation.Animation = null
 let mySprite: Sprite = null
 let goingRight = false
+let level = 0
+tiles.setCurrentTilemap(tilemap`level2`)
+level = 2
+let levels = [tilemap`level`, tilemap`level2`]
 goingRight = false
-tiles.setCurrentTilemap(tilemap`level`)
 mySprite = sprites.create(assets.image`PlayerIdle`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
 scene.cameraFollowSprite(mySprite)
 InitwalkLeft()
 initVertAnim()
+changeLevelFunctions()
 forever(function () {
     animatePlayer()
+})
+game.onUpdateInterval(500, function () {
+    for (let value of enemies) {
+        if (Math.percentChance(50)) {
+            if (Math.percentChance(50)) {
+                value.vx = 50
+            } else {
+                value.vx = -50
+            }
+            if (Math.percentChance(50)) {
+                value.vy = 50
+            } else {
+                value.vy = -50
+            }
+        }
+    }
 })
